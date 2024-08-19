1034fa99bc454f957ccfb866d7e154b3e70dd399
aleu7819 <allison.asl.leung@gmail.com>
2024-08-18 20:38:03 -0700

    Update Program5.java
    input data and equation

--- Program5.java
+++ Program5.java
@@ -2,6 +2,33 @@
 
 //Your code here
 
+public class Program5
+{public static void main (String[] args) {
+
+// import data
+int royaleMile=286;
+int royaleGallon=9;
+
+int koopaMile=412;
+int koopaGallon=40;
+
+int pipeMile=361;
+int pipeGallon=18;
+
+int badMile=161;
+int badGallon=11;
+
+// calculate average mpg - x10 into an int, divide it by 10 into a double?
+
+int royaleMpg = (royaleMile/royaleGallon)*10;
+
+
+//print average out
+    
+    
+    
+    
+}}
 //Paste console output below:
 /*
 
